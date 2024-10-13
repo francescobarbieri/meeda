49d64fe979378a27c5d98414b7ea5e856ad6e598
Francesco Barbieri <fbarbieri.business@gmail.com>
2024-10-13 19:34:37 +0200

    fixing shadcn/ui styling not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from "@/components/ui/button"
-import { EnvelopeOpenIcon } from '@radix-ui/react-icons'
+import { Button } from '@/components/ui/button';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-        <Button>
-            <EnvelopeOpenIcon className='mr-2 h-4 w-4' />
-            Click Me
-        </Button>
+      <Button variant="destructive">Click Me</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite';
+import path from 'path';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        outDir: 'dist',
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, 'src'),
-        },
-    },
+  },
 });
