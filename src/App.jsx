b4470a8c9a7936ebc24c57b9e7d65cdb9a53d81a
Francesco Barbieri <fbarbieri.business@gmail.com>
2024-10-13 23:07:27 +0200

    messing with charts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Button } from '@/components/ui/button';
+import TestChart from './components/charts/networth';
 
 function App() {
   return (
     <>
       <Button variant="destructive">Click Me</Button>
+      <TestChart />
     </>
   );
 }
---
+++ src/components/charts/test.jsx
@@ -0,0 +1,7 @@
+const Test = () => {
+  return (
+    <p className="bg-red-400">Heyla!</p>
+  );
+}
+ 
+export default Test;
